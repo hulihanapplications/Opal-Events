57b7ca874389bb0ed5d99acabb557047180a7fa3
Dave Hulihan <dave@hulihanapplications.com>
2011-06-25 14:51:32 -0600

    Autoload everything in app/models

--- lib/opal_events.rb
+++ lib/opal_events.rb
@@ -1,1 +1,3 @@
-require File.join(File.dirname(__FILE__), "..", "app", "models", "plugin_event") 
+Dir.glob(File.join(File.dirname(__FILE__), "..", "app", "models", "*.rb")).each do |file|
+  require file
+end
